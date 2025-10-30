fa9bf3b18a811283ca78a638606ee2adc85ce90e
Aaryan Sharma <aaryanradhe8@gmail.com>
2025-10-31 00:08:44 +0530

    implemented dockerization using docker compose

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,17 +5,14 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import queryClient from './config/queryClient.ts'
 import { Provider } from './components/ui/provider.tsx'
-import SocketContext from './context/SocketContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <SocketContext>
-      <QueryClientProvider client={queryClient}>
-        <Provider>
-          <App />
-        </Provider>
-        <ReactQueryDevtools initialIsOpen={false} buttonPosition='bottom-right' position='right'/>
-      </QueryClientProvider>
-    </SocketContext>
+  <StrictMode>  
+    <QueryClientProvider client={queryClient}>
+      <Provider>
+        <App />
+      </Provider>
+      <ReactQueryDevtools initialIsOpen={false} buttonPosition='bottom-right' position='right'/>
+    </QueryClientProvider>
   </StrictMode>,
 )
